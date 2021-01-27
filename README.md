c58c7467d65c62a62e237a17701afec012a08351
Doug <phena109@gmail.com>
2021-01-27 12:44:54 +1100

    Getting the input

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Installation
 
-1. Clone the repository  
+1. Clone the repository
 
-``git clone https://github.com/phena109/network-path-test.git``
+    ``git clone https://github.com/phena109/network-path-test.git``
 
-2. Install the dependency 
+2. Enter the repo
    
-``composer install``
+    ``cd network-path-test``
+   
+3. Install the dependency
+
+    ``composer install``
 
 3. Run the program
 
-``php run.php``
+    ``php run.php``
--- src/App.php
+++ src/App.php
@@ -3,6 +3,9 @@
 
 namespace phena109;
 
+use Exception;
+use Hoa\Console\Readline\Readline;
+use InvalidArgumentException;
 use Iterator;
 use League\Csv\Reader;
 
@@ -18,6 +21,41 @@ class App
         foreach ($paths as $path) {
             echo implode(',', $path) . "\n";
         }
+
+        echo "\n";
+
+        $this->process();
+    }
+
+    private function process()
+    {
+        $rl = new Readline();
+        do {
+            $input = $rl->readLine("What is the input: ");
+            echo "\n";
+
+            try {
+                $parsed = $this->parseInput($input);
+                echo implode(' ', $parsed) . "\n";
+            } catch (Exception $e) {
+                echo "\nPath not found\n";
+            }
+        } while (false !== $input && 'quit' !== strtolower($input));
+    }
+
+    private function parseInput(string $input)
+    {
+        $_input = strtoupper(trim($input));
+        preg_match_all("/^([A-Z]\s+[A-Z]\s+[0-9]+)$/", $_input, $parsed);
+
+        if (count($parsed) != 2) {
+            throw new InvalidArgumentException('Invalid input');
+        }
+        if (count($parsed[1]) == 0) {
+            throw new InvalidArgumentException('Invalid input');
+        }
+
+        return $parsed[1];
     }
 
     private function getPaths(): Iterator
